602e92b8d77ae9737369b02908b69c7989191a03
Andrea Linares <linares.andrea@correo.unimet.edu.ve>
2024-03-08 15:13:43 -0400

    :thread: Router + Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router.jsx';
+import './index.css';
+import App from './App';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router}>
+      
+
     <App />
+      
+    </RouterProvider>
   </React.StrictMode>,
 )
--- src/router.jsx
+++ src/router.jsx
@@ -1,20 +1,34 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+import HomePage  from './pages/HomePage/HomePage';
+import  LoginPage  from './pages/LogInPage/LoginPage';
+import  SignInPage from './pages/SignInPage/SignInPage';
+import UserPage from './pages/UserPage/UserPage';
+import  ClubDetails  from './pages/ClubDetails/ClubDetails';
+import  SearchPage  from './pages/SearchPage/SearchPage';
+
 export const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <HomePage />,
-    },
-    {
-      path: '/home/:id/:nombre',
-      element: <ParamsPage />,
-    },
-    {
-      path: '/login',
-      element: <LoginPage />,
-    },
-    {
-      path: '/user/profile',
-      element: <div>Perfil de usuario</div>,
-    },
-  ]);
-  
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/clubes',
+    element: <ClubDetails />,
+  },
+  {
+    path: '/login',
+    element: <LoginPage />,
+  },
+  {
+    path: '/signIn',
+    element: <SignInPage />,
+  },
+  {
+    path: '/user/profile',
+    element: <UserPage />,
+  },
+  {
+    path: '/search',
+    element: <SearchPage />,
+  }
+]);
