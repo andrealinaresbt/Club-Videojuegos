3d23fde9f54247e4392ff8d570e7557997b5cb44
GabrielDBrito <britogabrielc@gmail.com>
2024-03-10 18:10:49 -0400

    ClubDetails

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,33 @@
 /* eslint-disable react/prop-types */
 import styles from "./Card.module.css"
+import { useNavigate } from "react-router-dom";
+import { CLUB_URL } from "../../constants/urls";
 
 export  function Card(props) {
+  const { name, description, games } = props;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(CLUB_URL, {
+      state: {
+        name,
+        description,
+        games,
+      },
+    });
+  };
+
   return (
     <div className={styles.card}>
         <h1 className={styles.title}>{props.name}</h1>
         <h4>{props.description}</h4>
         <h5>Juegos: {props.games}</h5>
         <div className={styles.buttons}>
-          <button className={styles.button}>Más información</button>
+          <button onClick={handleClick} className={styles.button}>
+            Más información
+          </button>
           <button className={styles.button}>Unirse</button>
-        </div>
-        
+        </div>      
     </div>
   )
 }
--- src/pages/ClubDetails/ClubDetails.jsx
+++ src/pages/ClubDetails/ClubDetails.jsx
@@ -1,7 +1,29 @@
+import styles from "./ClubDetails.module.css";
+import { useLocation } from "react-router-dom";
 
 
 export default function ClubDetails() {
+  const location = useLocation();
+  const { name, description, games } = location.state || {};
+  console.log(name+'nombre');
+
   return (
-    <div>ClubDetails</div>
+    <main>  
+        <section className={styles.middlebox}>
+        <h1 className={styles.title}>{name}</h1>
+              <div className={styles.info}>
+                <div>
+                  <h4 className={styles.description}>{description}</h4>
+                  <h4 className={styles.description}>Juegos: {games}</h4>
+                </div>
+                <div className={styles.buttonContainer}>
+                  <button className={styles.button}>Unirse</button>
+                </div>
+              </div>
+              
+        </section>
+        
+        
+    </main>
   )
 }
